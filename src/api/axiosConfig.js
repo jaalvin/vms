86f8277a88d9cd3ea4419c7f0e02bee2eba6f01a
jaalvin <turkson.aj.alvin@gmail.com>
2026-03-29 20:21:53 +0000

    Revert to stable version

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://visitor-management-system-backend-zag1.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
